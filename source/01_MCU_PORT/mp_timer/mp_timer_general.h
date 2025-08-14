4303617f704cf9efd1d23689b73f2264998e3336
Naim <naeemmas@gmail.com>
2025-08-14 09:34:41 +0300

    feat(MCU_PORT/timer): ✨ features and imporvements
    Move typedefs used in function arguments to improve maintainability and
    reduce errors could happen between confilicts at upper layers
    Implement hard delay in us and ms and reading cpu time in us and ms also
    by using the general purpose timer as the timer increment every 1 us

--- source/01_MCU_PORT/mp_timer/mp_timer_general.h
+++ source/01_MCU_PORT/mp_timer/mp_timer_general.h
@@ -4,15 +4,6 @@
 
 #include "ha_timer/ha_timer_private.h"
 
-typedef enum
-{
-    MP_TIMER_10US_ID = 0,
-    MP_TIMER_1MS_ID,
-    MP_TIMER_10MS_ID,
-    MP_TIMER_100MS_ID,
-    MP_TIMER_CNT
-} mp_timer_id_t;
-
 timer_driver_t* timer_driver_register(void);
 
 #endif // MP_TIMER_GENERAL_H
--- source/02_HW_API/ha_timer/ha_timer_private.h
+++ source/02_HW_API/ha_timer/ha_timer_private.h
@@ -7,6 +7,21 @@
 
 typedef struct st_timer_driver_ifc* timer_driver_ifc;
 
+typedef enum
+{
+    MP_TIMER_10US_ID = 0,
+    MP_TIMER_1MS_ID,
+    MP_TIMER_10MS_ID,
+    MP_TIMER_100MS_ID,
+    MP_TIMER_CNT
+} mp_timer_id_t;
+
+typedef enum
+{
+    MP_TIMER_UNIT_US = 0,
+    MP_TIMER_UNIT_MS,
+} mp_timer_unit_t;
+
 typedef struct st_timer_driver
 {
     timer_driver_ifc api;
@@ -25,20 +40,20 @@ struct st_timer_driver_ifc
     /**
      * @brief This function shall start the hardware timer.
      *
-     * @param[in] p_timer_id The ID of the timer to start @ref mp_timer_id_t.
+     * @param[in] p_timer_id The ID of the timer to start
      *
      * @retval `RET_OK` if the timer is started successfully, else error code.
      */
-    response_status_t (*start)(uint8_t);
+    response_status_t (*start)(mp_timer_id_t);
 
     /**
      * @brief This function shall stop the hardware timer.
      *
-     * @param[in] p_timer_id The ID of the timer to stop @ref mp_timer_id_t.
+     * @param[in] p_timer_id The ID of the timer to stop
      *
      * @retval `RET_OK` if the timer is stopped successfully, else error code.
      */
-    response_status_t (*stop)(uint8_t);
+    response_status_t (*stop)(mp_timer_id_t);
 
     /**
      * @brief This function shall get the current state of the timer.
@@ -63,14 +78,30 @@ struct st_timer_driver_ifc
     /**
      * @brief This function shall register a callback for a specific timer event.
      *
-     * @param[in] p_cb_id The ID of the callback to register @ref mp_timer_callback_id_t.
+     * @param[in] p_timer_id The ID of the timer to register the callback for
      * @param[in] callback The callback function to register.
      *
      * @retval `RET_OK` if the callback is registered successfully, else error code.
      */
-    response_status_t (*register_callback)(uint8_t, void (*callback)(void));
+    response_status_t (*register_callback)(mp_timer_id_t, void (*callback)(void));
 
-    void (*hard_delay)(uint32_t);
+    /**
+     * @brief This function shall provide a hard delay in milliseconds or microseconds.
+     *
+     * @param[in] p_delay The delay duration.
+     * @param[in] p_delay_unit The unit of the delay, @ref mp_timer_unit_t.
+     *
+     */
+    void (*hard_delay)(uint32_t, mp_timer_unit_t);
+
+    /**
+     * @brief This function shall get the current CPU time in milliseconds or microseconds.
+     *
+     * @param[in] p_time_unit The unit of the time, @ref mp_timer_unit_t.
+     *
+     * @retval The current CPU time in the specified unit.
+     */
+    uint32_t (*get_cpu_time)(mp_timer_unit_t);
 };
 
 #endif /* HA_TIMER_PRIVATE_H */
